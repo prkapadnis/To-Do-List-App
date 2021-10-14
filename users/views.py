2ade52ae9a5f6927032f0970bdf3c0f31f3d0410
prkapadnis <prkapadnis2001@gmail.com>
2021-10-14 12:38:15 +0530

    User registration and Authentication system created

---
+++ users/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+from .forms import UserRegistration
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('/Note/home')
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(request, username=form.cleaned_data.get(
+                'username'), password=form.cleaned_data.get('password'))
+            if user is not None:
+                login(request, user)
+                return redirect('/Note/home')
+            else:
+                messages.error(request, f"Invalid Username or Password!")
+        else:
+            messages.error(request, f"Invalid Username or Password!")
+    else:
+        form = AuthenticationForm()
+    context = {'form': form}
+    return render(request, "user/login_view.html", context)
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserRegistration(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Successfully registered with the {form.instance.username} username')
+    else:
+        form = UserRegistration()
+    context = {'form': form}
+    return render(request, 'user/register_view.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('/login')
